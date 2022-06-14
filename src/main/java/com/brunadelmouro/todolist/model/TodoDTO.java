c0875f5be24fbfbec3e47d36a83ea63b65b82c56
Bruna <brunadelmouro@gmail.com>
2022-06-14 15:09:10 -0300

    Validating with Spring Validation

--- src/main/java/com/brunadelmouro/todolist/model/TodoDTO.java
+++ src/main/java/com/brunadelmouro/todolist/model/TodoDTO.java
@@ -8,8 +8,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
@@ -21,9 +20,15 @@ public class TodoDTO {
     @Id
     private String id;
 
+    @NotNull(message = "todo cannot be null")
     private String todo;
+
+    @NotNull(message = "description cannot be null")
     private String description;
+
+    @NotNull(message = "completed cannot be null")
     private Boolean completed;
+
     private String createdAt;
     private String updatedAt;
 }
