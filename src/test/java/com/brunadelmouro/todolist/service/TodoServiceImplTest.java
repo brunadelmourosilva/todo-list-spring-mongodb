a63dda2339e54909d11caf6c8fa12bc11aacdb29
Bruna <brunadelmouro@gmail.com>
2022-06-17 12:18:13 -0300

    Service test - get all todos

--- src/test/java/com/brunadelmouro/todolist/service/TodoServiceImplTest.java
+++ src/test/java/com/brunadelmouro/todolist/service/TodoServiceImplTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,21 +26,42 @@ class TodoServiceImplTest {
     @InjectMocks
     TodoServiceImpl todoService;
 
-    TodoDTO todoDTO;
+    TodoDTO todoDTO1;
+    TodoDTO todoDTO2;
+
+    List<TodoDTO> todos;
     String nowDate;
 
     @BeforeEach
     void setUp() {
         nowDate = String.valueOf(System.currentTimeMillis());
-        todoDTO = new TodoDTO("62a7c51c541e3655708d6544", "Study algorithms and data structures", "Study COM112", false, nowDate, nowDate);
+
+        todoDTO1 = new TodoDTO("62a7c51c541e3655708d6544", "Study algorithms and data structures", "Study COM112", false, nowDate, nowDate);
+        todoDTO2 = new TodoDTO("62a7c51c541e3655708d0000", "Study OOP", "Study COM220", true, nowDate, nowDate);
+
+        todos = List.of(todoDTO1, todoDTO2);
     }
 
     @Test
     void createTodo() {
+
     }
 
     @Test
     void getAllTodos() {
+
+        //given
+        given(todoRepository.findAll()).willReturn(todos);
+
+        //when
+        List<TodoDTO> responseList = todoService.getAllTodos();
+
+        //then
+        then(todoRepository).should(times(1)).findAll();
+
+        assertEquals(2, responseList.size());
+        assertEquals("Study algorithms and data structures", todos.get(0).getTodo());
+        assertEquals("Study OOP", todos.get(1).getTodo());
     }
 
     @Test
@@ -47,13 +69,13 @@ class TodoServiceImplTest {
         //chamar as funções da camada mais inferior para a mais superior
 
         //given - dado uma condição
-        given(todoRepository.findById(todoDTO.getId())).willReturn(Optional.of(todoDTO));
+        given(todoRepository.findById(todoDTO1.getId())).willReturn(Optional.of(todoDTO1));
 
         //when - quando tal condição acontecer, guarda o resultado de acordo com o retorno do método
-        TodoDTO response = todoService.getSingleTodo(todoDTO.getId());
+        TodoDTO response = todoService.getSingleTodo(todoDTO1.getId());
 
         //then - então garanta que os os resultados foram iguais
-        then(todoRepository).should(times(1)).findById(todoDTO.getId());
+        then(todoRepository).should(times(1)).findById(todoDTO1.getId());
 
         //verificando se
         assertEquals("Study algorithms and data structures", response.getTodo());
@@ -66,15 +88,15 @@ class TodoServiceImplTest {
     void getSingleTodoWhereIdWasNotFound() {
 
         //given
-        given(todoRepository.findById(todoDTO.getId())).willReturn(Optional.ofNullable(null));
+        given(todoRepository.findById(todoDTO1.getId())).willReturn(Optional.ofNullable(null));
 
         //when
         assertThrows(TodoCollectionException.class, () -> {
-            todoService.getSingleTodo(todoDTO.getId());
+            todoService.getSingleTodo(todoDTO1.getId());
         });
 
         //then
-        then(todoRepository).should(times(1)).findById(todoDTO.getId());
+        then(todoRepository).should(times(1)).findById(todoDTO1.getId());
     }
 
     @Test
