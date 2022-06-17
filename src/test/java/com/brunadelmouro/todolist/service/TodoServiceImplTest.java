95fd63d6f22749236c39db7e5943166342e4949f
Bruna <brunadelmouro@gmail.com>
2022-06-17 11:14:00 -0300

    Service test - id was found with success

---
+++ src/test/java/com/brunadelmouro/todolist/service/TodoServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.brunadelmouro.todolist.service;
+
+import com.brunadelmouro.todolist.exception.TodoCollectionException;
+import com.brunadelmouro.todolist.model.TodoDTO;
+import com.brunadelmouro.todolist.repository.TodoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceImplTest {
+
+    @Mock
+    TodoRepository todoRepository;
+
+    @InjectMocks
+    TodoServiceImpl todoService;
+
+    TodoDTO todoDTO;
+    String nowDate;
+
+    @BeforeEach
+    void setUp() {
+        nowDate = String.valueOf(System.currentTimeMillis());
+        todoDTO = new TodoDTO(null, "Study algorithms and data structures", "Study COM112", false, nowDate, nowDate);
+    }
+
+    @Test
+    void createTodo() {
+    }
+
+    @Test
+    void getAllTodos() {
+    }
+
+    @Test
+    void getSingleTodoWhereIdWasFound() throws TodoCollectionException {
+        //chamar as funções da camada mais inferior para a mais superior
+
+        //given - dado uma condição
+        given(todoRepository.findById(todoDTO.getId())).willReturn(Optional.of(todoDTO));
+
+        //when - quando tal condição acontecer, guarda o resultado de acordo com o retorno do método
+        TodoDTO response = todoService.getSingleTodo(todoDTO.getId());
+
+        //then - então garanta que os os resultados foram iguais
+        then(todoRepository).should(times(1)).findById(todoDTO.getId());
+
+        //verificando se
+        assertEquals("Study algorithms and data structures", response.getTodo());
+        assertEquals("Study COM112", response.getDescription());
+        assertEquals(false, response.getCompleted());
+        assertEquals(nowDate, response.getCreatedAt());
+    }
+
+    @Test
+    void updateTodo() {
+    }
+
+    @Test
+    void deleteTodoById() {
+    }
+}
